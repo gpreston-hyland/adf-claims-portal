34b435a3464d917fa9aa38d157b06a34c6649f5e
Galen Preston <114751324+gpreston-hyland@users.noreply.github.com>
2023-09-22 18:47:11 -0500

    eob - 2023-09-22

--- src/app/services/global-values.service.ts
+++ src/app/services/global-values.service.ts
@@ -8,8 +8,14 @@ export class GlobalValuesService {
   currentUser:string = '';
   policyBaseURL:string = 'http://ec2-18-207-201-97.compute-1.amazonaws.com:4000/policy';
   claimBaseURL:string = 'http://ec2-18-207-201-97.compute-1.amazonaws.com:4000/claim';
-  appName:string = 'claims-comm-auto-mvp';
+  
+  appName:string = 'claims-test';    //'claims-comm-auto-mvp';
+  fnolProcessDefinitionId = 'Process_z-vNQpyK';   //'Process_LIvxCBO2';
   fnolProcess:string = 'first-notice-of-loss';
+  fnolUploadTaskName = 'upload File(s)';
+  claimNumber:string = '12345';
+  companyName:string = 'Ace Hardware';
+  claimFolderName:string = '-root-/Sites/claims/documentLibrary/Commercial Auto/Acme/12345';
   loggedIn:boolean;
 }
 
--- src/app/start-claim/start-claim.component.ts
+++ src/app/start-claim/start-claim.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
-import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 import {GlobalValuesService} from '../services/global-values.service';
-import { ProcessInstanceCloud, ProcessPayloadCloud, StartProcessCloudService} from '@alfresco/adf-process-services-cloud';
+
+import { ProcessInstanceCloud, ProcessPayloadCloud, StartProcessCloudService,
+         TaskListCloudService,
+         TaskQueryCloudRequestModel} from '@alfresco/adf-process-services-cloud';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-start-claim',
@@ -15,65 +19,122 @@ export class StartClaimComponent implements OnInit {
   bStartProcess: boolean = false;
   bShowResults: boolean = false;
   bShowTaskForm: boolean = false;
-  isLoading: boolean = true;
-  formTaskId: String;
+  bIsLoading: boolean = true;
+  processId: string;
+  formTaskId: string;
 
   results:any ;
 
   constructor(public globalValues:GlobalValuesService
       , private _router:Router
       ,private _startProcCloud:StartProcessCloudService
+      ,private _taskListCloudService:TaskListCloudService
     ) {    console.log('******************************* startclaimcomponent-constructor'); }
 
   ngOnInit(): void {
     console.log('******************************* startclaimcomponent-ngOnInit');
     //Start new process
-    this.isLoading = true;
+    this.bIsLoading = true;
     this.startNewClaimProcess();
 
   }
 
+  // ***********************************************************
+  //  Claim Process functions
+  // ***********************************************************
+
   startNewClaimProcess() {
     console.debug("********* enter startNewClaimProcess()");
 
-    var _payload: ProcessPayloadCloud;
+    var _payload: ProcessPayloadCloud = new ProcessPayloadCloud();
     
-    _payload = new ProcessPayloadCloud(); 
+    // TODO: call create claim folder process
+    // see https://stackoverflow.com/questions/22125865/how-to-wait-until-a-predicate-condition-becomes-true-in-javascript
+    // to monitor for task completion before moving on
+
+
+    // _payload = new ProcessPayloadCloud(); 
     //Process_z-vNQpyK:42:cdb9bb1b-5885-11ee-86f3-7ab301120a4c
-   _payload.processDefinitionKey = 'Process_z-vNQpyK'; //this.globalValues.fnolProcess;
-   _payload.name = 'FNOL-Service-1'; //Build unique name
-   _payload.variables = {claimId:"12345", companyName:"Ace Hardware", claimFolderName:"-root-/Sites/claims/documentLibrary/Commercial Auto/Acme/12345"};
+   _payload.processDefinitionKey = this.globalValues.fnolProcessDefinitionId;
+   _payload.name = this.globalValues.fnolProcess.concat("-",this.globalValues.currentUser,"-",this.globalValues.claimNumber);    //'FNOL-Service-1'; //Build unique name
+   _payload.variables = {//claimId:this.globalValues.claimNumber,
+     companyName:this.globalValues.companyName, 
+     claimFolderName:this.globalValues.claimFolderName};
    _payload.payloadType = 'StartProcessPayload';
 //    this._startProcCloud.startProcess(this.globalValues.appName,_payload).subscribe((task:ProcessInstanceCloud) => {
-    this._startProcCloud.startProcess('claims-test',_payload).subscribe((task:ProcessInstanceCloud) => {
-        this.isLoading = false;
+    this._startProcCloud.startProcess(this.globalValues.appName,_payload).subscribe((task:ProcessInstanceCloud) => {
+        // this.bIsLoading = false;
         console.log(task);
-        this.formTaskId = task.id;
+        this.processId = task.id;
         this.results = JSON.stringify(task);
-        this.bShowTaskForm = true;
+        timer(1000).subscribe(x => {
+          this.getFormTask();
+        });
+        
+        // this.bShowTaskForm = true;
       },
       error =>{ 
         console.error("-------------- *************** Error starting proc with cloud service");
-        this.isLoading = false;
+        this.bIsLoading = false;
         this.results = 'Error starting proc';
         this.bShowResults = true;
         }
-    );
-
-    
+    );   
   }
 
+  // ***********************************************************
+  //  User Task Form functions
+  // ***********************************************************
+  getFormTask() {
+    // console.debug("********************************** " + this.globalValues.appName);
+
+     var _formTaskQuery:TaskQueryCloudRequestModel = new TaskQueryCloudRequestModel();
+    //  console.debug("-------------------------------- " + this.globalValues.appName);     
+     _formTaskQuery.appName = this.globalValues.appName;
+     _formTaskQuery.processInstanceId = this.processId;
+     _formTaskQuery.name = this.globalValues.fnolUploadTaskName;
+
+    
+
+    console.log("============================================ begin getFormTask");
+    this._taskListCloudService.getTaskByRequest(_formTaskQuery)
+      .pipe(map(task => task.list.entries[0] ))
+        .subscribe(
+          (task) => {
+          this.bIsLoading = false;
+          console.debug("******************************** form taskid:".concat(task.id));
+          this.formTaskId = task.id;
+          this.bShowTaskForm = true;
+          // this.results = JSON.stringify(task);
+          // this.bShowResults = true;
+          },
+          error => {
+            console.error("-------------- *************** Error searching for user task from process: ".concat(this.processId));
+            this.bIsLoading = false;
+            this.results = 'Error search for user task from process:'.concat(this.processId);
+            this.bShowResults = true;
+          }
+        );
+    // 
+
+  }
 
   taskFormCompleted() {
     this.bShowTaskForm = false;
+    alert("Claim documents submitted for review.");
+    this._router.navigateByUrl('/home');
   }
   
-  taskFormCancel() {
+  taskFormCancel(event: string) {
+    console.debug("======================== User cancelled form. Event returned:".concat(event));
+
+    // TODO: Need to cancel / delete parent Process  (this.processId)
     this.bShowTaskForm = false;
+    this._router.navigateByUrl('/');
   }
 
   taskFormError(event: any) {
-    console.error("************ Task Form ERROR: " + event);
+    console.error("************ Task Form ERROR: ".concat(event));
     this.bShowTaskForm = false; 
     this.results = JSON.stringify(event);
     this.bShowResults = true;
